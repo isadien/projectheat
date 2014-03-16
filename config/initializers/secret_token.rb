d80e19dc4d3abdf7add8305bc06df50c52c6a6ad
Irwyn Sadien <isadien@umflint.edu>
2014-03-16 19:03:48 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectheat::Application.config.secret_key_base = 'dfb60e6809fa5fc63719af5cdfd93cae2be0fd9b61cf2f3e6e9c383d47a8c65f0ae040384e6d16a0dfac108cf4144e612301d0a8f14278a5caa9a1aef21065e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Projectheat::Application.config.session_store :cookie_store, key: '_projectheat_session'
